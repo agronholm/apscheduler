ea2e04d66477b650c4f8e690242186efedb125fa
agronholm <devnull@localhost>
2010-03-21 06:49:36 +0200

    Moved long description to README.txt, updated version number

--- README.txt
+++ README.txt
@@ -1,5 +1,32 @@
-Advanced Python Scheduler (C) Alex Grönholm
+APScheduler is a light but powerful in-process task scheduler that
+lets you schedule functions (or any python callables) to be executed at times
+of your choosing.
 
-This is an in-process task scheduling library for Python.
-See the project homepage (http://apscheduler.nextday.fi/)
-for documentation and installation instructions.
+The development of APScheduler was heavily influenced by the `Quartz
+<http://www.opensymphony.com/quartz/>`_ task scheduler written in Java,
+although APScheduler cannot claim as many features.
+
+
+Features
+========
+
+* No external dependencies
+* Thread-safe API
+* Cron-like scheduling
+* Delayed scheduling of single fire jobs (like the UNIX "at" command)
+* Interval-based scheduling of jobs, with configurable start date and
+  repeat count
+
+
+Documentation
+=============
+
+Documentation can be found on the `APScheduler site
+<http://apscheduler.nextday.fi/>`_.
+
+
+Source
+======
+
+The source can be browsed at `Bitbucket
+<http://bitbucket.org/agronholm/apscheduler/src/>`_.
--- setup.py
+++ setup.py
@@ -7,45 +7,11 @@ except ImportError:
     except ImportError:
         from distutils.core import setup
 
-
 setup(
     name='APScheduler',
-    version='1.1',
+    version='1.2',
     description='In-process task scheduler with Cron-like capabilities',
-    long_description="""\
-APScheduler is a light but powerful in-process task scheduler that
-lets you schedule functions (or any python callables) to be executed at times
-of your choosing.
-
-The development of APScheduler was heavily influenced by the `Quartz
-<http://www.opensymphony.com/quartz/>`_ task scheduler written in Java,
-although APScheduler cannot claim as many features.
-
-
-Features
-========
-
-* No external dependencies
-* Thread-safe API
-* Cron-like scheduling
-* Delayed scheduling of single fire jobs (like the UNIX "at" command)
-* Interval-based scheduling of jobs, with configurable start date and
-  repeat count
-
-
-Documentation
-=============
-
-Documentation can be found on the `APScheduler site
-<http://apscheduler.nextday.fi/>`_.
-
-
-Source
-======
-
-The source can be browsed at `Bitbucket
-<http://bitbucket.org/agronholm/apscheduler/src/>`_.
-""",
+    long_description=open('README.txt').read(),
     author='Alex Gronholm',
     author_email='apscheduler@nextday.fi',
     url='http://apscheduler.nextday.fi/',
