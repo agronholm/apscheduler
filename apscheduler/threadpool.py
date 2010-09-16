210fb99afdd4fea5459534bd26ca869ae4a58a6c
agronholm <devnull@localhost>
2010-09-16 09:13:43 +0300

    Thread pools now handle interpreter exits gracefully, thanks to advice from Brian Quinlan

--- apscheduler/threadpool.py
+++ apscheduler/threadpool.py
@@ -3,11 +3,23 @@ Generic thread pool class. Modeled after Java's ThreadPoolExecutor.
 """
 
 from Queue import Queue, Empty
-from threading import Thread, Lock
+from threading import Thread, Lock, currentThread
 import logging
+import atexit
 
 
 logger = logging.getLogger(__name__)
+_threadpools = set()
+
+# Worker threads are daemonic in order to let the interpreter exit without
+# an explicit shutdown of the thread pool. The following trick is necessary
+# to allow worker threads to finish cleanly.
+def _shutdown_all():
+    for pool in tuple(_threadpools):
+        pool.shutdown()
+
+atexit.register(_shutdown_all)
+
 
 class ThreadPool(object):
     def __init__(self, core_threads=0, max_threads=None, keepalive=1):
@@ -18,17 +30,19 @@ class ThreadPool(object):
         :param keepalive: seconds to keep non-core worker threads waiting
             for new tasks
         """
-        self.queue = Queue()
-        self.threads_lock = Lock()
-        self.num_threads = 0
-        self.busy_threads = 0
         self.core_threads = core_threads
         self.max_threads = max_threads
         self.keepalive = keepalive
+        self._queue = Queue()
+        self._threads_lock = Lock()
+        self._threads = set()
+        self._busy_threads = 0
+        self._shutdown = False
 
-        if max_threads:
+        if max_threads is not None:
             self.max_threads = max(max_threads, core_threads, 1)
 
+        _threadpools.add(self)
         logger.info('Started thread pool with %d core threads and %s maximum '
                     'threads', core_threads, max_threads or 'unlimited')
 
@@ -37,17 +51,12 @@ class ThreadPool(object):
         t = Thread(target=self._run_jobs, args=(core,))
         t.setDaemon(True)
         t.start()
-        self.num_threads += 1
-
-    def _add_threadcount(self, increment):
-        self.threads_lock.acquire()
-        self.num_threads += increment
-        self.threads_lock.release()
+        self._threads.add(t)
 
     def _add_busycount(self, increment):
-        self.threads_lock.acquire()
-        self.busy_threads += increment
-        self.threads_lock.release()
+        self._threads_lock.acquire()
+        self._busy_threads += increment
+        self._threads_lock.release()
 
     def _run_jobs(self, core):
         logger.debug('Started worker thread')
@@ -59,8 +68,11 @@ class ThreadPool(object):
 
         while True:
             try:
-                func, args, kwargs = self.queue.get(block, timeout)
+                func, args, kwargs = self._queue.get(block, timeout)
             except Empty:
+                break
+
+            if self._shutdown:
                 break
 
             self._add_busycount(1)
@@ -70,15 +82,49 @@ class ThreadPool(object):
                 logger.exception('Error in worker thread')
             self._add_busycount(-1)
 
-        self._add_threadcount(-1)
+        self._threads_lock.acquire()
+        self._threads.discard(currentThread())
+        self._threads_lock.release()
+
         logger.debug('Exiting worker thread')
 
+    @property
+    def num_threads(self):
+        return len(self._threads)
+
+    @property
+    def busy_threads(self):
+        return self._busy_threads
+
     def execute(self, func, args=(), kwargs={}):
-        self.threads_lock.acquire()
+        if self._shutdown:
+            raise Exception('Thread pool has already been shut down')
+
+        self._threads_lock.acquire()
         try:
-            self.queue.put((func, args, kwargs))
+            self._queue.put((func, args, kwargs))
             if (self.busy_threads == self.num_threads and not
                 self.max_threads or self.num_threads < self.max_threads):
                 self._add_thread()
         finally:
-            self.threads_lock.release()
+            self._threads_lock.release()
+
+    def shutdown(self, wait=True):
+        if self._shutdown:
+            return
+
+        self._shutdown = True
+        logging.info('Shutting down thread pool')
+
+        self._threads_lock.acquire()
+        for _ in range(len(self._threads)):
+            self._queue.put((None, None, None))
+        self._threads_lock.release()
+
+        if wait:
+            for thread in tuple(self._threads):
+                thread.join()
+
+    def __repr__(self):
+        return '<ThreadPool at %x; threads=%d busy=%d>' % (id(self),
+            len(self._threads), self._busy_threads)
--- tests/apschedulertests/testthreadpool.py
+++ tests/apschedulertests/testthreadpool.py
@@ -1,12 +1,43 @@
 from threading import Event
 from time import sleep
 
-from nose.tools import eq_
+from nose.tools import eq_, assert_raises
 
 from apscheduler.threadpool import ThreadPool
 
 
 def test_threadpool():
+    pool = ThreadPool(core_threads=2, keepalive=0)
+    event1 = Event()
+    event2 = Event()
+    event3 = Event()
+    pool.execute(event1.set)
+    pool.execute(event2.set)
+    pool.execute(event3.set)
+    event1.wait(1)
+    event2.wait(1)
+    event3.wait(1)
+    assert event1.isSet()
+    assert event2.isSet()
+    assert event3.isSet()
+    sleep(1)
+    eq_(pool.busy_threads, 0)
+    eq_(pool.num_threads, 2)
+
+    eq_(repr(pool), '<ThreadPool at %x; threads=2 busy=0>' % id(pool))
+
+    pool.shutdown()
+    eq_(pool.busy_threads, 0)
+    eq_(pool.num_threads, 0)
+
+    # Make sure double shutdown is ok
+    pool.shutdown()
+
+    # Make sure one can't submit tasks to a thread pool that has been shut down
+    assert_raises(Exception, pool.execute, event1.set)
+
+
+def test_threadpool_nocore():
     pool = ThreadPool(keepalive=0)
     event = Event()
     pool.execute(event.set)
@@ -15,18 +46,3 @@ def test_threadpool():
     sleep(1)
     eq_(pool.busy_threads, 0)
     eq_(pool.num_threads, 0)
-
-
-def test_threadpool_corethreads():
-    pool = ThreadPool(core_threads=1, keepalive=0)
-    event1 = Event()
-    event2 = Event()
-    pool.execute(event1.set)
-    pool.execute(event2.set)
-    event1.wait(1)
-    event2.wait(1)
-    assert event1.isSet()
-    assert event2.isSet()
-    sleep(1)
-    eq_(pool.busy_threads, 0)
-    eq_(pool.num_threads, 1)
