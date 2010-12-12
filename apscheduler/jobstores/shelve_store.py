1e589fd4fd1a2a8c6a216e0ea3e355feb6db5d97
agronholm <devnull@localhost>
2010-12-12 06:51:55 +0200

    shelve.open() needs to be called with flags='c' in all cases

--- apscheduler/jobstores/shelve_store.py
+++ apscheduler/jobstores/shelve_store.py
@@ -10,16 +10,14 @@ from apscheduler.jobstores.base import JobStore
 from apscheduler.util import obj_to_ref
 
 
-def store(mode):
-    def outer(func):
-        def wrapper(self, *args, **kwargs):
-            store = shelve.open(self.path, mode, self.protocol)
-            try:
-                return func(self, store, *args, **kwargs)
-            finally:
-                store.close()
-        return wrapper
-    return outer
+def store(func):
+    def wrapper(self, *args, **kwargs):
+        store = shelve.open(self.path, 'c', self.protocol)
+        try:
+            return func(self, store, *args, **kwargs)
+        finally:
+            store.close()
+    return wrapper
 
 
 class ShelveJobStore(JobStore):
@@ -37,23 +35,23 @@ class ShelveJobStore(JobStore):
             if not id in store:
                 return id
 
-    @store('c')
+    @store
     def add_job(self, store, job):
         job.func_ref = obj_to_ref(job.func)
         job.id = self._generate_id(store)
         self.jobs.append(job)
         store[job.id] = job
 
-    @store('w')
+    @store
     def update_job(self, store, job):
         store[job.id] = job
 
-    @store('w')
+    @store
     def remove_job(self, store, job):
         del store[job.id]
         self.jobs.remove(job)
 
-    @store('r')
+    @store
     def load_jobs(self, store):
         self.jobs = store.values()
 
