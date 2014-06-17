6f85cefcf8444044da25b5b029d325397a78eb8c
Alex Grönholm <alex.gronholm@nextday.fi>
2014-06-18 01:39:48 +0300

    Bumped version to 3.0.0b1

--- apscheduler/__init__.py
+++ apscheduler/__init__.py
@@ -1,5 +1,5 @@
-version_info = (3, 0, 0, 'pre1')
-version = '.'.join(str(n) for n in version_info[:3])
-release = '.'.join(str(n) for n in version_info)
+version_info = (3, 0, 0, 'b1')
+version = '3.0.0'
+release = '3.0.0b1'
 
-__version__ = version  # PEP 396
+__version__ = release  # PEP 396
