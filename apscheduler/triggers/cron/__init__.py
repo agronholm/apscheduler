225b49d12771dc76a21d1f7e2836c13e84a4868e
liuchao12 <liuchao12@meituan.com>
2015-11-25 16:04:55 +0800

    fix CronTrigger.get_next_fire_time() bug: calculate start_date problem

--- apscheduler/triggers/cron/__init__.py
+++ apscheduler/triggers/cron/__init__.py
@@ -133,9 +133,9 @@ class CronTrigger(BaseTrigger):
 
     def get_next_fire_time(self, previous_fire_time, now):
         if previous_fire_time:
-            start_date = max(now, previous_fire_time + timedelta(microseconds=1))
+            start_date = min(now, previous_fire_time + timedelta(microseconds=1))
         else:
-            start_date = max(now, self.start_date) if self.start_date else now
+            start_date = min(now, self.start_date) if self.start_date else now
 
         fieldnum = 0
         next_date = datetime_ceil(start_date).astimezone(self.timezone)
