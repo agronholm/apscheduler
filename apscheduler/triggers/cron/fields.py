ad9109042b727fe1366b7f1b10382766a9a3469f
Alex Grönholm <alex.gronholm@nextday.fi>
2017-11-26 22:43:57 +0200

    Fixed the maximum value for CronTrigger's "year" field
    datetime.date() does not take years higher than 9999.

--- apscheduler/triggers/cron/fields.py
+++ apscheduler/triggers/cron/fields.py
@@ -13,7 +13,7 @@ __all__ = ('MIN_VALUES', 'MAX_VALUES', 'DEFAULT_VALUES', 'BaseField', 'WeekField
 
 MIN_VALUES = {'year': 1970, 'month': 1, 'day': 1, 'week': 1, 'day_of_week': 0, 'hour': 0,
               'minute': 0, 'second': 0}
-MAX_VALUES = {'year': 2 ** 63, 'month': 12, 'day:': 31, 'week': 53, 'day_of_week': 6, 'hour': 23,
+MAX_VALUES = {'year': 9999, 'month': 12, 'day:': 31, 'week': 53, 'day_of_week': 6, 'hour': 23,
               'minute': 59, 'second': 59}
 DEFAULT_VALUES = {'year': '*', 'month': 1, 'day': 1, 'week': '*', 'day_of_week': '*', 'hour': 0,
                   'minute': 0, 'second': 0}
