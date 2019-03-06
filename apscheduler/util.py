4ba6aa0e31ec915d39b33f63c0b899798de5d91e
Egor Malykh <fnk@fea.st>
2019-03-06 22:24:31 +0300

    fix signature inspection for wrapped function

--- apscheduler/util.py
+++ apscheduler/util.py
@@ -332,7 +332,7 @@ def check_callable_args(func, args, kwargs):
     has_varargs = has_var_kwargs = False
 
     try:
-        sig = signature(func)
+        sig = signature(func, follow_wrapped=False)
     except ValueError:
         # signature() doesn't work against every kind of callable
         return
