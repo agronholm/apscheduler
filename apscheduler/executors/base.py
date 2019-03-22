d277b6e017efe3a6782cd85d2f986701f4c2c295
Robin Frick <robin.frick@n26.com>
2019-03-22 19:28:08 +0100

    line length

--- apscheduler/executors/base.py
+++ apscheduler/executors/base.py
@@ -128,7 +128,8 @@ def run_job(job, jobstore_alias, run_times, logger_name):
             exc, tb = sys.exc_info()[1:]
             formatted_tb = ''.join(format_tb(tb))
             events.append(JobExecutionEvent(EVENT_JOB_ERROR, job.id, jobstore_alias, run_time,
-                                            actual_start_time, exception=exc, traceback=formatted_tb))
+                                            actual_start_time, exception=exc,
+                                            traceback=formatted_tb))
             logger.exception('Job "%s" raised an exception', job)
 
             # This is to prevent cyclic references that would lead to memory leaks
