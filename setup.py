b10c20905e79c6fb4c87fab1b76401b57915b3a7
Alex Grönholm <alex.gronholm@nextday.fi>
2020-09-27 01:23:32 +0300

    Removed leftover configuration from setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        'version_scheme': 'post-release',
-        'local_scheme': 'dirty-tag'
-    },
-    setup_requires=[
-        'setuptools >= 36.2.7',
-        'setuptools_scm >= 1.7.0'
-    ]
-)
+setup()
