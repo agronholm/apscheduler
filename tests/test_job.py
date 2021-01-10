341a25d2f870c3e681ce4f0cc85ee275d77f9ddc
Bar Harel <bzvi7919@gmail.com>
2021-01-11 01:21:21 +0200

    Allow Job to be weak-referenced (#470)

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import gc
+import weakref
 from datetime import datetime, timedelta
 from functools import partial
 
@@ -62,6 +64,14 @@ def test_resume(job):
 def test_remove(job):
     job.remove()
     job._scheduler.remove_job.assert_called_once_with(job.id, None)
+
+
+def test_weakref(create_job):
+    job = create_job(func=dummyfunc)
+    ref = weakref.ref(job)
+    del job
+    gc.collect()
+    assert ref() is None
 
 
 def test_pending(job):
