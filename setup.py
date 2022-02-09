13420163f1ebcd6253f4134660c44d5f1b0fc312
Alex Grönholm <alex.gronholm@nextday.fi>
2022-02-09 23:46:03 +0200

    Added classifiers for Python 3.8 to 3.10

--- setup.py
+++ setup.py
@@ -28,7 +28,10 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
     keywords='scheduling cron',
     license='MIT',
