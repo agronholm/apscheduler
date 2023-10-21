2b79c07cbdfecae4fc644487bfe5d0bc8e6b0f42
Alex Grönholm <alex.gronholm@nextday.fi>
2023-10-21 17:30:13 +0300

    Use psycopg instead of psycopg2 use in examples

--- examples/web/wsgi_flask.py
+++ examples/web/wsgi_flask.py
@@ -2,7 +2,7 @@
 Example demonstrating use with WSGI (raw WSGI application, no framework).
 
 Requires the "postgresql" and "redis" services to be running.
-To install prerequisites: pip install sqlalchemy psycopg2 flask uwsgi
+To install prerequisites: pip install sqlalchemy psycopg flask uwsgi
 To run: uwsgi -T --http :8000 --wsgi-file wsgi_flask.py
 
 It should print a line on the console on a one-second interval while running a
@@ -33,7 +33,7 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-engine = create_engine("postgresql+psycopg2://postgres:secret@localhost/testdb")
+engine = create_engine("postgresql+psycopg://postgres:secret@localhost/testdb")
 data_store = SQLAlchemyDataStore(engine)
 event_broker = RedisEventBroker.from_url("redis://localhost")
 scheduler = Scheduler(data_store, event_broker)
--- examples/web/wsgi_noframework.py
+++ examples/web/wsgi_noframework.py
@@ -2,7 +2,7 @@
 Example demonstrating use with WSGI (raw WSGI application, no framework).
 
 Requires the "postgresql" and "redis" services to be running.
-To install prerequisites: pip install sqlalchemy psycopg2 uwsgi
+To install prerequisites: pip install sqlalchemy psycopg uwsgi
 To run: uwsgi -T --http :8000 --wsgi-file wsgi_noframework.py
 
 It should print a line on the console on a one-second interval while running a
@@ -35,7 +35,7 @@ def application(environ, start_response):
     return [response_body]
 
 
-engine = create_engine("postgresql+psycopg2://postgres:secret@localhost/testdb")
+engine = create_engine("postgresql+psycopg://postgres:secret@localhost/testdb")
 data_store = SQLAlchemyDataStore(engine)
 event_broker = RedisEventBroker.from_url("redis://localhost")
 scheduler = Scheduler(data_store, event_broker)
