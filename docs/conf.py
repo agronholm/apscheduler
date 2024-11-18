e8bf1432a8b2da366f54bad77a65f3ec040888e0
Alex Grönholm <alex.gronholm@nextday.fi>
2024-11-19 01:48:03 +0200

    Used the sphinx_rtd_theme extension in the Sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import apscheduler
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
