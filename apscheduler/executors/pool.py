f791e3dc60143570264771f9907e703d2d0b320e
Alex Grönholm <alex.gronholm@nextday.fi>
2024-11-19 02:33:44 +0200

    Unconditionally set mp_context for ProcessPoolExecutor

--- apscheduler/executors/pool.py
+++ apscheduler/executors/pool.py
@@ -1,5 +1,5 @@
 import concurrent.futures
-import sys
+import multiprocessing
 from abc import abstractmethod
 from concurrent.futures.process import BrokenProcessPool
 
@@ -73,12 +73,6 @@ class ProcessPoolExecutor(BasePoolExecutor):
 
     def __init__(self, max_workers=10, pool_kwargs=None):
         pool_kwargs = pool_kwargs or {}
-
-        # On Python 3.14, "spawn" is the default mp_context
-        if sys.version_info < (3, 14):
-            import multiprocessing
-
-            pool_kwargs.setdefault("mp_context", multiprocessing.get_context("spawn"))
-
+        pool_kwargs.setdefault("mp_context", multiprocessing.get_context("spawn"))
         pool = concurrent.futures.ProcessPoolExecutor(int(max_workers), **pool_kwargs)
         super().__init__(pool)
