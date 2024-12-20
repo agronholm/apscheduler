e026f46a144e747c4685032f76945b7d7ddbd90b
HomerusJa <jakob@schluse.com>
2024-12-21 00:36:21 +0100

    Add tests for cooldown_period being applied correctly

--- tests/triggers/test_combining.py
+++ tests/triggers/test_combining.py
@@ -200,11 +200,32 @@ class TestOrTrigger:
         # The end time of the 6 second interval has been reached
         assert trigger.next() is None
 
+    def test_cooldown_period(self, timezone, serializer):
+        start_time = datetime(2020, 5, 16, 14, 17, 30, 254212, tzinfo=timezone)
+        trigger = OrTrigger(
+            [
+                IntervalTrigger(seconds=4, start_time=start_time),
+                IntervalTrigger(seconds=6, start_time=start_time),
+            ],
+            cooldown_period=1,
+        )
+        if serializer:
+            trigger = serializer.deserialize(serializer.serialize(trigger))
+
+        assert trigger.next() == start_time
+        assert trigger.next() == start_time + timedelta(seconds=4)
+        assert trigger.next() == start_time + timedelta(seconds=6)
+        assert trigger.next() == start_time + timedelta(seconds=8)
+        assert trigger.next() == start_time + timedelta(seconds=12)  # No second trigger -> cooldown
+        assert trigger.next() == start_time + timedelta(seconds=16)
+        assert trigger.next() == start_time + timedelta(seconds=18)
+
     def test_repr(self, timezone):
         date1 = datetime(2020, 5, 16, 14, 17, 30, 254212, tzinfo=timezone)
         date2 = datetime(2020, 5, 18, 15, 1, 53, 940564, tzinfo=timezone)
-        trigger = OrTrigger([DateTrigger(date1), DateTrigger(date2)])
+        trigger = OrTrigger([DateTrigger(date1), DateTrigger(date2)], 1)
+        print(repr(trigger))
         assert repr(trigger) == (
             "OrTrigger([DateTrigger('2020-05-16 14:17:30.254212+02:00'), "
-            "DateTrigger('2020-05-18 15:01:53.940564+02:00')])"
+            "DateTrigger('2020-05-18 15:01:53.940564+02:00')], cooldown_period=1.0)"
         )
