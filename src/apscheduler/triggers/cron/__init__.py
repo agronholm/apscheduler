f1f97393c9796eded8c196993d6ced8d7edd8016
Jonas Sitzmann <jonas.sitzmann@energymeteo.de>
2025-07-01 17:41:33 +0200

    - remove default argument strict=False from `zip` call

--- src/apscheduler/triggers/cron/__init__.py
+++ src/apscheduler/triggers/cron/__init__.py
@@ -100,7 +100,7 @@ class CronTrigger(Trigger):
         self._fields = []
         assigned_values = {
             field_name: value
-            for (field_name, _), value in zip(self.FIELDS_MAP, values, strict=False)
+            for (field_name, _), value in zip(self.FIELDS_MAP, values)
             if value is not None
         }
         for field_name, field_class in self.FIELDS_MAP:
